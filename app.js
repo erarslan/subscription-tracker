f28033a93f216401a4717213b72665b737222565
Emre Erarslan <emrerars@gmail.com>
2025-04-02 18:56:32 +0300

    signup controller

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware);
 
